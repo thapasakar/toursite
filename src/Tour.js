a7c98b5b9245b99cca2d1548714c47fc4a8293bd
thapasakar <thapasakar2@gmail.com>
2024-06-02 20:00:30 -0400

    first commit

---
+++ src/Tour.js
@@ -0,0 +1,10 @@
+const Tour()=>{
+
+    return(
+        <div>
+        <h1>This is the single tour component</h1>
+        </div>
+    )
+}
+
+export default Tour
---
+++ src/Tours.js
@@ -0,0 +1,10 @@
+const Tours()=>{
+return(
+    <div>
+        <h1>Tours component</h1>
+    </div>
+
+)
+}
+
+export default Tours
